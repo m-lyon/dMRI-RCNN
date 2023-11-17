1ff5c0476528f5f0d97a961da20213ff2fd5dc65
Matthew Lyon <32886639+m-lyon@users.noreply.github.com>
2023-11-17 12:46:28 +0000

    Update setup.py
    Fix to homepage and download links

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ scripts = [
     'dmri_rcnn/bin/dmri_sh_baseline.py',
 ]
 
-version = '0.4.0'
+version = '0.4.1'
 this_dir = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_dir, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
@@ -31,8 +31,8 @@ setup(
     description='Diffusion MRI Recurrent CNN for Angular Super-resolution.',
     author='Matthew Lyon',
     author_email='matthewlyon18@gmail.com',
-    url='https://https://github.com/m-lyon/dMRI-RCNN',
-    download_url=f'https://https://github.com/m-lyon/dMRI-RCNN/archive/v{version}.tar.gz',
+    url='https://github.com/m-lyon/dMRI-RCNN',
+    download_url=f'https://github.com/m-lyon/dMRI-RCNN/archive/v{version}.tar.gz',
     long_description=long_description,
     long_description_content_type='text/markdown',
     python_requires='>=3.8',
